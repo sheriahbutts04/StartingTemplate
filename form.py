256b4e0ffc2502203a3de38b913dc12e2529522e
Sheriah Butts <93352427+sheriahbutts04@users.noreply.github.com>
2022-01-20 23:26:34 +0000

    made 2 lists

--- form.py
+++ form.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-class MessageForm(FlaskForm):
-    message = StringField('Enter your to do list')
+class SchoolForm(FlaskForm):
+    message = StringField('Enter your school list')
     submit = SubmitField('Send')
 
+class HomeForm(FlaskForm):
+    message = StringField('Enter your home list')
+    submit = SubmitField('Send')
   
     
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
-from form import MessageForm
+from form import SchoolForm
+from form import HomeForm
 from config import Config
 from todo import my_function
 
@@ -9,40 +10,57 @@ app = Flask(
 )
 app.config.from_object(Config)
 
-my_message = []
-def setMessage(message):
-  global my_message
-  my_message.append(message)
-  print(my_message)
+my_school_list = []
+def set_school_message(message):
+  global my_school_list
+  my_school_list.append(message)
+  print(my_school_list)
+
+my_home_list = []
+def set_home_message(message):
+  global my_home_list
+  my_home_list.append(message)
+  print(my_home_list)
 
 def my_function():
   print("Todolist")
 
 @app.route('/', methods=['GET', 'POST'])
 def page_one():
-  form = MessageForm()
+  form1 = SchoolForm()
   my_function()
-  if form.is_submitted():
-    print("made it")
-    setMessage(form.message.data)
-    return redirect('/display')
-  return render_template('pageOne.html', form=form)
+  form2 = HomeForm()
+  if form1.validate_on_submit():
+    if form1.is_submitted():
+      print("made it")
+      set_school_message(form1.message.data)
+      return redirect('/display')
+  elif form2.validate_on_submit():
+    if form2.is_submitted():
+      print("made it")
+      set_home_message(form2.message.data)
+      return redirect('/display')
+  return render_template('pageOne.html', form1=form1,form2=form2)
 
   
 @app.route('/display')
 def page_two():
-  return render_template('pageTwo.html', my_message=my_message)
+  return render_template('pageTwo.html', my_school_list=my_school_list)
+
+@app.route('/display')
+def page2_two():
+  return render_template('pageTwo.html', my_home_list=my_home_list)
 
 app.run(host='0.0.0.0', port=8080)
 
-@app.route('/onetwo', methods=['GET', 'POST'])
-def page_one_two():
-  form = MessageForm()
-  if form.is_submitted():
-    print("made it")
-    setMessage(form.message.data)
-    return redirect('/display')
-  return render_template('pageOne.html', form=form)
+# @app.route('/onetwo', methods=['GET', 'POST'])
+# def page_one_two():
+#   form = HomeForm()
+#   if form.is_submitted():
+#     print("made it")
+#     set_home_message(form.message.data)
+#     return redirect('/display')
+#   return render_template('pageOne.html', form=form)
   
 app.run(host='0.0.0.0',port=8080)
 
