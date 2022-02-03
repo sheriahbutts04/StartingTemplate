0e3bc6850a5623830870909627ffb6afde97ae41
Sheriah Butts <93352427+sheriahbutts04@users.noreply.github.com>
2022-02-03 22:13:47 +0000

    ee

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
 from form import MessageForm
 from config import Config
-from todo import my_function
+
 
 app = Flask('app', template_folder='templates')
 app.config.from_object(Config)
@@ -9,7 +9,7 @@ app.config.from_object(Config)
 my_school_list = []
 
 
-def set_school_message(message):
+def set_message(message):
     global my_school_list
     my_school_list.append(message)
     print(my_school_list)
@@ -18,11 +18,10 @@ def set_school_message(message):
 @app.route('/', methods=['GET', 'POST'])
 def page_one():
     form = MessageForm()
-    my_function()
     if form.validate_on_submit():
         if form.is_submitted():
             print("made it")
-            set_school_message(form.message.data)
+            set_message(form.message.data)
             return redirect('/display')
     return render_template('pageOne.html', form=form)
 
--- todo.py
+++
@@ -1,8 +0,0 @@
-def my_function():
-  Todolist = []
-  add = input("what would you like to do")
-  Todolist.append(add)
-  print(Todolist)
-  #add = input("what would you like to remove")
-
-
